87311d8acbda4017722ff5af6bca57d565972128
Vincenzo Musco <muscovin@gmail.com>
2017-09-25 17:07:26 -0400

    Creating a function for metrics computation

--- metrics.py
+++ metrics.py
@@ -45,37 +45,18 @@ def skipThisClass(skips, clazz):
 
     return False
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Compute evolution metrics between two smali versions.')
-    parser.add_argument('smaliv1', type=str,
-                        help='Version 1 folder containing smali files')
-    parser.add_argument('smaliv2', type=str,
-                        help='Version 2 folder containing smali files')
-    parser.add_argument('pkg', type=str,
-                        help='The app package name')
-    parser.add_argument('--verbose', '-v', action='store_true',
-                        help='Show metrics details')
-    parser.add_argument('--classes', type=str, nargs='*',
-                        help='Include only these classes')
-
-    args = parser.parse_args()
-
-
-
+def computeMetrics(v1, v2, pkg):
     old = smali.SmaliProject.SmaliProject()
-    old.parseFolder(args.smaliv1, args.pkg)
+    old.parseFolder(v1, pkg)
     new = smali.SmaliProject.SmaliProject()
-    new.parseFolder(args.smaliv2, args.pkg)
+    new.parseFolder(v2, pkg)
 
-    counted = 0
-
-    E,R,C = 0,0,0
-    MA,MD,MC = 0,0,0
-    FA,FD,FC = 0,0,0
-    CA,CD,CC = 0,0,0
+    E, R, C = 0, 0, 0
+    MA, MD, MC = 0, 0, 0
+    FA, FD, FC = 0, 0, 0
+    CA, CD, CC = 0, 0, 0
 
     r = old.differences(new, [])
-
 
     for rr in r:
         if args.classes is not None:
@@ -85,7 +66,7 @@ if __name__ == '__main__':
         if rr[1] is None:
             # Class change level here...
             if rr[0][1] is None:
-                CD +=1
+                CD += 1
                 continue
             elif rr[0][0] is None:
                 CA += 1
@@ -104,7 +85,6 @@ if __name__ == '__main__':
         if isChange(l):
             C += 1
 
-
         for rrr in rr[1]:
             if rrr[0] is not None and rrr[0].isField() and rrr[1] is None:
                 FD += 1
@@ -119,11 +99,30 @@ if __name__ == '__main__':
             elif rrr[0] is not None and rrr[0] is not None and rrr[0].isMethod():
                 MC += 1
 
+    return len(old.classes), len(new.classes), E,R,C,CA,CD,CC,MA,MD,MC,FA,FD,FC
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Compute evolution metrics between two smali versions.')
+    parser.add_argument('smaliv1', type=str,
+                        help='Version 1 folder containing smali files')
+    parser.add_argument('smaliv2', type=str,
+                        help='Version 2 folder containing smali files')
+    parser.add_argument('pkg', type=str,
+                        help='The app package name')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='Show metrics details')
+    parser.add_argument('--classes', type=str, nargs='*',
+                        help='Include only these classes')
+
+    args = parser.parse_args()
+
+    oldclasses,newclasses,E,R,C,MA,MD,MC,FA,FD,FC,CA,CD,CC = computeMetrics(args.smaliv1, args.smaliv2, args.pkg)
+
     if args.verbose:
-        print("v0 has %d classes, v1 has %d classes."%(len(old.classes), len(new.classes)))
+        print("v0 has %d classes, v1 has %d classes."%(oldclasses, newclasses))
         print("E = %d. R = %d. C = %d" % (E, R, C))
         print("Classes - Added: %5d, Changed: %5d, Deleted: %5d." % (CA, CC, CD))
         print("Methods - Added: %5d, Changed: %5d, Deleted: %5d." % (MA, MC, MD))
         print(" Fields - Added: %5d, Changed: %5d, Deleted: %5d." % (FA, FC, FD))
     else:
-        print("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d"%(len(old.classes), len(new.classes), E,R,C,CA,CD,CC,MA,MD,MC,FA,FD,FC))
+        print("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d"%(oldclasses, newclasses, E,R,C, MA,MD,MC, FA,FD,FC, CA,CD,CC))
