185abe44b2530a67f309db3f51381873cc222802
Vincenzo Musco <muscovin@gmail.com>
2017-11-21 15:58:14 -0500

    Adding InstanceState patternds 3 4a and 4b

--- faultsanalysis/InstanceStateAnalysis.py
+++ faultsanalysis/InstanceStateAnalysis.py
@@ -3,8 +3,11 @@
 # Date: November 17, 2017
 
 import re
+
+import sys
+
 import smali.SmaliProject
-import smali.SmaliInstructionMatchers
+import smali.SmaliInstructionMatchers as sim
 
 #https://developer.android.com/reference/android/app/Activity.html
 #https://developer.android.com/reference/android/app/Dialog.html
@@ -191,8 +194,149 @@ def findReadWriteMethods(type, methods):
 
     return {"load": loadInstanceDeclaration, "save": saveInstanceDeclaration}
 
+def findOnCreateMethods(methods):
+    ret = []
+
+    for m in methods:
+        if isThisMethods(m, 'onCreate', 'V', ['Landroid/os/Bundle;']) or isThisMethods(m, 'onCreate', 'V', ['Landroid/os/Bundle;', 'Landroid/os/PersistableBundle;']):
+            ret.append(m)
+
+    return ret
+
 def compareMethod(method, matcher):
     return method.name == matcher[1] and ''.join(method.params) == matcher[2] and method.ret == matcher[3]
+
+def isSuperInvoked(method):
+    for l in method.getCleanLines():
+        super = sim.matchSuperInvocation(l)
+
+        if super is not None and compareMethod(method, super):
+            return True
+
+    return False
+
+def isThereAnyBundleProtection(method):
+    for l in method.getCleanLines():
+        if l.startswith('if-nez p1, :') or l.startswith('if-eqz p1, :'):
+            return True
+
+    return False
+
+def listBundleAccesses(method):
+    ret = []
+
+    lastKey = {}
+
+    for l in method.getCleanLines():
+        #print(l)
+        match = sim.affectingString(l)
+
+        if match is not None:
+            lastKey[match[0]] = match[1]
+
+        match = sim.matchVirtualInvocationBundleAccesses(l)
+
+        if match is not None:
+            match = list(match)
+            match[0] = lastKey[match[0]]
+            ret.append(match)
+
+    if len(ret) == 0:
+        return None
+
+    return ret
+
+
+
+##########
+##########
+##
+##  PATTERNS IMPLEMENTATION
+##
+##########
+##########
+
+
+
+
+# PATTERN 3 --
+# Bundle data acces is protected by == null or != null check ?
+def bundleAccessIsProtectedByNullCheck(methods):
+    unprotectedMethods = []
+
+    for amethod in methods:
+        bac = listBundleAccesses(amethod)
+
+        if bac is not None:
+            # We access the bundle... So let's check if it is protected...
+            if not isThereAnyBundleProtection(amethod):
+                unprotectedMethods.append(bac)
+
+    return unprotectedMethods
+
+# PATTERN 4a --
+# Are all bundle item saved well loaded?
+def bundleReadWhatIsWritten(methods):
+    writeBundle = set()
+    readBundle = set()
+
+    for amethod in methods:
+        bac = listBundleAccesses(amethod)
+
+        if bac is not None:
+            for b in bac:
+                if b[1] == 'get':
+                    readBundle.add(b[0])
+                else:
+                    writeBundle.add(b[0])
+
+    # print("Bundle contains %d items..." % (max(len(readBundle), len(writeBundle))))
+    symdiff = readBundle ^ writeBundle
+
+    return list(symdiff)
+    #if len(symdiff) > 0:
+    #    print("Read/Write not done symmetricly. Incoherent items: %s." % ','.join(symdiff))
+
+# PATTERN 4b --
+# Writing/loading type sanity check?
+def bundleRespectTypeCoherencies(methods):
+    dict = {}
+    incoherent = set()
+
+    for amethod in methods:
+        bac = listBundleAccesses(amethod)
+
+        if bac is not None:
+            for b in bac:
+                if b[0] not in dict:
+                    dict[b[0]] = b[2]
+                else:
+                    if dict[b[0]] != b[2]:
+                        incoherent.add(b[0])
+
+    return list(incoherent)
+    #print("Sanity types: %s" % ('OK' if len(incoherent) == 0 else 'ERROR'))
+
+# PATTERN 2 -- 
+# Are we calling super methods?
+def superCalledOnAllMethods(methods):
+    #loadInvokeSuper = 0
+    #saveInvokeSuper = 0
+
+    missingSuper = []
+
+    for k in methods:
+        # Super invokation analysis
+        if not isSuperInvoked(k):
+            missingSuper.append(k)
+            #if k in loadInstanceDeclaration:
+            #    loadInvokeSuper += 1
+            #else:
+            #    saveInvokeSuper += 1
+
+    #print('%s (%s). Load: %d/%d. Save: %d/%d.' % (clazz.getBaseName().replace('/', '.'), rootparent.split('/')[-1], loadInvokeSuper, len(loadInstanceDeclaration), saveInvokeSuper, len(saveInstanceDeclaration)))
+    return missingSuper
+
 
 if __name__ == '__main__':
 
@@ -200,11 +344,12 @@ if __name__ == '__main__':
     path = "/Users/vince/Downloads/Kontak9"
     path = "/Users/vince/Temp/SW_072516d"           #V1
     path = "/Users/vince/Temp/SW_5b66df5"           #V2
+    path = "/Users/vince/Temp/SW_master"           #V2
 
     project = smali.SmaliProject.SmaliProject()
     project.parseFolder(path)
 
-
+    print('Class, P3_bundle_access_protection, P4a_all_read_write, P4b_type_check, P2_super_calling')
 
     for dentry in findClassesOfInterests(project):
         clazz = dentry["class"]
@@ -216,35 +361,28 @@ if __name__ == '__main__':
         loadInstanceDeclaration = loadSave["load"]
         saveInstanceDeclaration = loadSave["save"]
 
-        loadInvokeSuper = 0
-        saveInvokeSuper = 0
+        #print(clazz.name, '\n')
 
-        for k in loadInstanceDeclaration:
-            invokeSuper = False
-            for l in k.getCleanLines():
-                super = smali.SmaliInstructionMatchers.matchSuperInvocation(l)
+        sys.stdout.write(clazz.getBaseName())
+        sys.stdout.write(',')
 
-                if super is not None and compareMethod(k, super):
-                    invokeSuper = True
-                    #print('\t\t %s'%l)
+        # PATTERN 3 -- Bundle data acces is protected by == null or != null check ?
+        r = bundleAccessIsProtectedByNullCheck(findOnCreateMethods(loadInstanceDeclaration))
+        sys.stdout.write(':'.join(r))
+        sys.stdout.write(',')
 
-            if invokeSuper:
-                loadInvokeSuper += 1
 
-            #print('\t- %s(%s). Invoke super = %s'%(k.name, ','.join(k.params), invokeSuper))
+        # PATTERN 4a -- Are all bundle item saved well loaded?
+        r = bundleReadWhatIsWritten(loadInstanceDeclaration + saveInstanceDeclaration)
+        sys.stdout.write(':'.join(r))
+        sys.stdout.write(',')
 
-        for k in saveInstanceDeclaration:
-            invokeSuper = False
-            for l in k.getCleanLines():
-                super = smali.SmaliInstructionMatchers.matchSuperInvocation(l)
+        # PATTERN 4b -- Writing/loading type sanity check?
+        r = bundleRespectTypeCoherencies(loadInstanceDeclaration + saveInstanceDeclaration)
+        sys.stdout.write(':'.join(r))
+        sys.stdout.write(',')
 
-                if super is not None and compareMethod(k, super):
-                    invokeSuper = True
-                    #print('\t\t %s'%l)
-
-            if invokeSuper:
-                saveInvokeSuper += 1
-
-            #print('\t- %s(%s). Invoke super = %s'%(k.name, ','.join(k.params), invokeSuper))
-
-        print('%s (%s). Load: %d/%d. Save: %d/%d.' % (clazz.getBaseName().replace('/', '.'), rootparent.split('/')[-1], loadInvokeSuper, len(loadInstanceDeclaration), saveInvokeSuper, len(saveInstanceDeclaration)))
+        # PATTERN 2 -- Are we calling super methods?
+        r = superCalledOnAllMethods(loadInstanceDeclaration + saveInstanceDeclaration)
+        sys.stdout.write(':'.join(['%s(%s)'%(rr.name, ''.join(rr.params)) for rr in r]))
+        sys.stdout.write('\n')
--- smali/SmaliInstructionMatchers.py
+++ smali/SmaliInstructionMatchers.py
@@ -4,8 +4,11 @@
 
 import re
 
-INVOKE_SUPER = re.compile("^invoke-super \{[pv0-9, ]*?\}, (.*)->(.*)\((.*)\)(.{1})$")
-INVOKE_VIRTUAL = re.compile("^invoke-virtual \{[pv0-9, ]*?\}, (.*)->(.*)\((.*)\)(.{1})$")
+INVOKE_SUPER = re.compile("^invoke-super \{[pv0-9, ]*?\}, (.*)->(.*)\((.*)\)(.*)$")
+INVOKE_VIRTUAL = re.compile("^invoke-virtual \{[pv0-9, ]*?\}, (.*)->(.*)\((.*)\)(.*)$")
+INVOKE_VIRTUAL_BUNDLE_ACCESSES = re.compile("^invoke-virtual \{p1, (v[0-9]+?), v[0-9]+?\}, Landroid/os/Bundle;->(get|put)(.*)\(.*\)(.*)$")
+INVOKE_VIRTUAL_BUNDLE_ACCESSES_ARRAY = re.compile("^invoke-virtual \{p1, (v[0-9]+?)\}, Landroid/os/Bundle;->(get|put)(.*)\(.*\)(.*)$")
+CONST_STRING_INSTRUCTION = re.compile("^const-string (v[0-9]+?), \"(.*)\"$")
 
 def matchSuperInvocation(smaliline):
     match  = INVOKE_SUPER.match(smaliline.strip())
@@ -18,6 +21,28 @@ def matchSuperInvocation(smaliline):
 
 def matchVirtualInvocation(smaliline):
     match  = INVOKE_VIRTUAL.match(smaliline.strip())
+
+    if match is None:
+        return None
+
+    return match.groups()
+
+
+def matchVirtualInvocationBundleAccesses(smaliline):
+    match  = INVOKE_VIRTUAL_BUNDLE_ACCESSES.match(smaliline.strip())
+
+    if match is None:
+        match = INVOKE_VIRTUAL_BUNDLE_ACCESSES_ARRAY.match(smaliline.strip())
+
+        if match is None:
+            return None
+
+    #print('>>> ', smaliline)
+    return match.groups()
+
+
+def affectingString(smaliline):
+    match = CONST_STRING_INSTRUCTION.match(smaliline.strip())
 
     if match is None:
         return None
